b281b29c2fae7eef43c2f12bb6aa0539b8b396d0
Juuso Saari <juusos93@gmail.com>
2021-07-13 12:45:28 +0300

    Update index.js
    Fixed bug

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const net = require('net')
-const WebSocketServer = require('ws').Server
+const net = require('net');
+const WebSocketServer = require('ws').Server;
 
+const log = msg => console.log(new Date(), msg);
 
 module.exports = (server, sockets) => {
-	let targets = []
+	let targets = [];
 	if(typeof(sockets) == "object"){
 		sockets.forEach(t => {
 			targets.push({
@@ -13,72 +14,64 @@ module.exports = (server, sockets) => {
 				port: t.target.split(':')[1],
 				connection: {},
 				path: t.path
-			})
-		})
-	}	 
+			});
+		});
+	};
 
 	let clientAddr = null;
 	for(let target of targets){
-		let p = target.path;
 
-		target.ws = new WebSocketServer({ noServer: true, p })
+		target.ws = new WebSocketServer({ noServer: true, ...target.path });
 
 		target.ws.on('connection', (client, req) => {
-			if(!clientAddr) clientAddr = client._socket.remoteAddress
-			console.log(req ? req.url : client.upgradeReq.url)
-			const log = msg => console.log(' ' + clientAddr + ': ' + msg)
-			// log('WebSocket connection from : ' + clientAddr)
-			// log('Version ' + client.protocolVersion + ', subprotocol: ' + client.protocol)
-			let ended = true;
-			if(target.connection._readableState){
-			  ended = target.connection._readableState.ended
-			}	
-			if(ended){
-				target.connection = net.createConnection(target.port, target.host, () => {
-					target.active = true;
-					log('connected to target')
-				})
-			}		
-			  
+			let cId = Date.now();
+			if(!clientAddr) clientAddr = client._socket.remoteAddress;
 
-			  target.connection.on('data', data => {
+			target.connection[cId] = net.createConnection(target.port, target.host, () => {
+			    log(`${clientAddr} -> Connected to target on ${target.host}:${target.port}`)
+			});	
+			  
+			target.connection[cId].on('data', data => {
 				try {
-				  client.send(data)
+					client.send(data)
 				} catch (e) {
-				  log("Client closed, cleaning up target")
-				  target.connection.end()
-				}
-			  })
-			  target.connection.on('end', () => {
-				log('target disconnected')
-				target.active = false;
-				client.close()
-			  })
-			  target.connection.on('error', () => {
-				log('target connection error')
-				target.connection.end()
-				client.close()
-			  })
-			  client.on('message', msg => {
-				target.connection.write(msg)
-			  })
-			  client.on('close', (code, reason) => {
-				log(`WebSocket client disconnected: ${code} [ ${reason} ]`)
-				target.connection.end()
-			  })
-			  client.on('error', error => {
-				log('WebSocket client error: ' + error)
-				target.connection.end()
-			  })
+					log(`${clientAddr} -> Client closed, cleaning up target`)					
+					target.connection[cId].end()
+				};
+			});
 			
+			target.connection[cId].on('end', () => {
+			    log(`${clientAddr} -> Target disconnected`)					
+				client.close()
+			});
 			
+			target.connection[cId].on('error', () => {
+			    log(`${clientAddr} -> Connection error`)					
+				target.connection[cId].end()
+				client.close()
+			});
+
+			client.on('message', msg => {
+				target.connection[cId].write(msg)
+			});
+
+			client.on('close', (code, reason) => {
+				log(`WebSocket client disconnected: ${code} [ ${reason} ]`);
+				target.connection[cId].end();
+			});
+
+			client.on('error', error => {
+			    log(`${clientAddr} -> WS Client error`);			
+				target.connection[cId].end();
+			});
+
 		});
 		server.on('upgrade', (request, socket, head) => {
 			if(request.url == target.path){
 				target.ws.handleUpgrade(request, socket, head, (ws) => {
 					target.ws.emit('connection', ws, request);
 				});
-			}
+			};
 		});
 	}
 }
